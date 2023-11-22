3f47fc91caa46157669cb1dc9ef3ce69f36e4fa4
Elisha <elishakibet67@gmail.com>
2023-11-22 12:56:13 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
+# Function 1
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+greet_programmer()
+
+# Function 2
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+greet("Elisha")
+
+# Function 3
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+greet_with_default()
+greet_with_default("Maureen")
+
+# Function 4
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+print(add(3, 4))
+
+# Function 5
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
+
+print(halve(6))
